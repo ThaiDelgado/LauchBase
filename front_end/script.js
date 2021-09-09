4fb262ec81561187ea204e08e11cf9809295c69f
ThaiDelgado <thaissadelgado@gmail.com>
2021-09-08 22:40:18 -0300

    console log

--- front_end/script.js
+++ front_end/script.js
@@ -3,5 +3,6 @@
 const modalOverlay = document.querySelector(".modal-overlay");
 const cards = document.querySelectorAll(".card");
 for(let card of cards){
+    console.log('object')
     card.addEventListener("click", function(){modalOverlay.classList.add("active")} )
 }
