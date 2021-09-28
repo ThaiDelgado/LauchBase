9753b5b1806705043607f00fc4ba4fc213a067c8
ThaiDelgado <thaissadelgado@gmail.com>
2021-09-28 00:29:07 -0300

    Cria rota

--- Backend/server.js
+++ Backend/server.js
@@ -4,3 +4,7 @@ const server = express()
 server.listen(5000, function(){
     console.log('server in running')
 })
+
+server.get("/", function(req, res){
+    return res.send("Meu Primeiro Servidor!")
+})
