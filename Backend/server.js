fa751a37f1226fffcfe3df73291af6c5437cacc5
ThaiDelgado <thaissadelgado@gmail.com>
2021-10-06 22:34:04 -0300

    Instala e config. nunjucks

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,13 @@
-
+const nunjucks = require('nunjucks')
 const express = require('express')
+
 const server = express()
+
+server.set("view engine", "html") //Configurando nunjucks
+
+nunjucks.configure("views", {  //Configurando nunjucks
+    express: server
+})
 
 server.listen(5000, function(){
     console.log('server in running')
